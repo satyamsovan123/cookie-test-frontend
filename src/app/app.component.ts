e91528c9bd2b49c8246baa4a679e6a0fb0ea1275
Satyam Sovan Mishra <33446554+satyamsovan123@users.noreply.github.com>
2024-08-09 18:31:58 +0530

    Add all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private httpClient: HttpClient) {}
   title = 'cookie-test-frontend';
+  message: string = 'Click on above buttons to test';
+  url = 'https://cookie-test-backend-qaba.onrender.com';
+
+  setCookie() {
+    try {
+      this.message = 'Loading';
+      this.httpClient
+        .get(this.url + '/set-cookie', { observe: 'response' })
+        .subscribe({
+          next: (response: any) => {
+            console.log(response.headers.get('access_token'));
+            this.message = response.body.message;
+          },
+          error: (error: any) => {
+            this.message = 'Error';
+            console.error(error.error);
+          },
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  verifyCookie() {
+    try {
+      this.message = 'Loading';
+      this.httpClient.get(this.url + '/verify-cookie').subscribe({
+        next: (response: any) => {
+          this.message = response.message;
+        },
+        error: (error: any) => {
+          this.message = 'Error';
+          console.error(error.error);
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  clearCookie() {
+    try {
+      this.message = 'Loading';
+      this.httpClient.get(this.url + '/clear-cookie').subscribe({
+        next: (response: any) => {
+          this.message = response.message;
+        },
+        error: (error: any) => {
+          this.message = 'Error';
+          console.error(error.error);
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  checkConnection() {
+    try {
+      this.message = 'Loading';
+      this.httpClient.get(this.url + '/api').subscribe({
+        next: (response: any) => {
+          this.message = response.message;
+        },
+        error: (error: any) => {
+          this.message = 'Error';
+          console.error(error.error);
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
