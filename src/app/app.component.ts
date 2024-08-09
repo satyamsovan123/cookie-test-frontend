ed3058a736bbd3d56748c2926de764a321b26be1
Satyam Sovan Mishra <33446554+satyamsovan123@users.noreply.github.com>
2024-08-09 19:25:10 +0530

    add all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   constructor(private httpClient: HttpClient) {}
-  deploymentMarker: number = 1;
+  deploymentMarker: number = 2;
   title = 'cookie-test-frontend';
   message: string = 'Click on above buttons to test';
   url = 'https://cookie-test-backend-qaba.onrender.com';
@@ -15,17 +15,15 @@ export class AppComponent {
   checkConnection() {
     try {
       this.message = 'Loading';
-      this.httpClient
-        .get(this.url + '/api', { withCredentials: true })
-        .subscribe({
-          next: (response: any) => {
-            this.message = response.message;
-          },
-          error: (error: any) => {
-            this.message = 'Error';
-            console.error(error.error);
-          },
-        });
+      this.httpClient.get(this.url + '/api').subscribe({
+        next: (response: any) => {
+          this.message = response.message;
+        },
+        error: (error: any) => {
+          this.message = 'Error';
+          console.error(error.error);
+        },
+      });
     } catch (error) {
       console.error(error);
     }
@@ -35,7 +33,10 @@ export class AppComponent {
     try {
       this.message = 'Loading';
       this.httpClient
-        .get(this.url + '/set-cookie', { observe: 'response' })
+        .get(this.url + '/set-cookie', {
+          withCredentials: true,
+          observe: 'response',
+        })
         .subscribe({
           next: (response: any) => {
             console.log(response.headers.get('access_token'));
@@ -54,15 +55,17 @@ export class AppComponent {
   verifyCookie() {
     try {
       this.message = 'Loading';
-      this.httpClient.get(this.url + '/verify-cookie').subscribe({
-        next: (response: any) => {
-          this.message = response.message;
-        },
-        error: (error: any) => {
-          this.message = 'Error';
-          console.error(error.error);
-        },
-      });
+      this.httpClient
+        .get(this.url + '/verify-cookie', { withCredentials: true })
+        .subscribe({
+          next: (response: any) => {
+            this.message = response.message;
+          },
+          error: (error: any) => {
+            this.message = 'Error';
+            console.error(error.error);
+          },
+        });
     } catch (error) {
       console.error(error);
     }
@@ -71,15 +74,17 @@ export class AppComponent {
   clearCookie() {
     try {
       this.message = 'Loading';
-      this.httpClient.get(this.url + '/clear-cookie').subscribe({
-        next: (response: any) => {
-          this.message = response.message;
-        },
-        error: (error: any) => {
-          this.message = 'Error';
-          console.error(error.error);
-        },
-      });
+      this.httpClient
+        .get(this.url + '/clear-cookie', { withCredentials: true })
+        .subscribe({
+          next: (response: any) => {
+            this.message = response.message;
+          },
+          error: (error: any) => {
+            this.message = 'Error';
+            console.error(error.error);
+          },
+        });
     } catch (error) {
       console.error(error);
     }
